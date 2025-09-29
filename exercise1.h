9e82f388df7c76d384e3d5bc3894810caff4804c
Hooda159 <160831635+Hooda159@users.noreply.github.com>
2025-09-29 21:29:07 +0700

    Add files via upload

---
+++ exercise1.h
@@ -0,0 +1,28 @@
+/*
+ * exercise1.h
+ *
+ *  Created on: Sep 29, 2025
+ *      Author: tongh
+ */
+
+#ifndef SRC_EXERCISE1_H_
+#define SRC_EXERCISE1_H_
+#include "main.h"
+
+unsigned int setIni = 0;
+unsigned int count = 0;
+void exercise1(){
+	if (setIni == 0){
+		HAL_GPIO_WritePin(red_led_GPIO_Port, red_led_Pin, 0);
+		HAL_GPIO_WritePin(yel_led_GPIO_Port, yel_led_Pin, 1); //set initial LED
+		setIni ++;
+	}
+	if (count >= 3){
+		count = 0;
+		HAL_GPIO_TogglePin(red_led_GPIO_Port, red_led_Pin);
+		HAL_GPIO_TogglePin(yel_led_GPIO_Port, yel_led_Pin);
+	}
+	else ++count;
+}
+
+#endif /* SRC_EXERCISE1_H_ */
---
+++ exercise5.h
@@ -0,0 +1,47 @@
+/*
+ * exercise5.h
+ *
+ *  Created on: Sep 29, 2025
+ *      Author: tongh
+ */
+
+#ifndef SRC_EXERCISE5_H_
+#define SRC_EXERCISE5_H_
+#include "exercise3.h"
+#include "exercise4.h"
+#include "main.h"
+void display7SEG2(int num){
+	if (num >= 0 && num <= 9){
+		for (int state = 7; state < 14; state++){
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 << state, arr[num - 1][state]);
+		}
+	}
+}
+void exercise5(){
+	exercise_3();
+	switch(ver_stage){
+	case 1:
+		display7SEG(3-ver_timer);
+		break;
+	case 2:
+		display7SEG(2-ver_timer);
+		break;
+	case 3:
+		display7SEG(5-ver_timer);
+		break;
+	}
+	switch (hor_stage){
+	case 1:
+		display7SEG2(5-hor_timer);
+		break;
+	case 2:
+		display7SEG2(3-hor_timer);
+		break;
+	case 3:
+		display7SEG2(2-hor_timer);
+		break;
+	}
+}
+
+
+#endif /* SRC_EXERCISE5_H_ */
